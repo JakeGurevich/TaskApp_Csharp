716578dd651f2f3b3306de6aafb0bed77db226ec
Jake Gurevich <umanrh@gmail.com>
2021-11-29 13:09:10 +0200

    Work from last week

--- TestAppTask/UnitTest1.cs
+++ TestAppTask/UnitTest1.cs
@@ -1,10 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using TaskApp.Classes;
+using TaskApp.Interfaces;
+
 namespace TestAppTask
 {
     [TestClass]
     public class UnitTest1
     {
+        IEditable _testHandler;
+
+        public UnitTest1()
+        {
+            var x = new Mock<IEditable>();
+            x.Setup(mockReader => mockReader.AddItem(new Item("new task")));
+            _testHandler = x.Object;
+        }
+
         [TestMethod]
         public void CheckItemName()
         {
@@ -12,5 +24,12 @@ namespace TestAppTask
             var item = new Item("learn");
             Assert.AreEqual(checkName,item.Title);
         }
+        [TestMethod]
+        public void AddItem_Check()
+        {
+            _testHandler;
+            Assert.AreEqual(checkName, item.Title);
+        }
+
     }
 }
